bc017c3799889f3dc88b54829d54e98d7a5d485a
Magdalena H. Selingerová <selingerova.magdalena@gmail.com>
2020-10-23 15:10:52 +0200

    pokus lišta

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ console.log('funguju');
 
 //přidat na kliknutí do pole kolečko
 
-/*const circle = event.target.classList.add('board__field--circle');
-const cross = event.target.classList.add('board__field--cross');  */
+/*const circle = event.target.classList.add('board__field--circle');*/
+
+/* const cross = document
+  .querySelector('.info')
+  .removeClass('info-kolecko')
+  .addClass('info-krizek'); */
+
 let player = false;
 const Play = (event) => {
   if (player) {
@@ -20,3 +25,13 @@ const btnElm = document.querySelectorAll('#btn_play');
 for (let i = 0; i < btnElm.length; i += 1) {
   btnElm[i].addEventListener('click', Play);
 }
+const Info = (event) => {
+  if (player) {
+    event.target.classList.add('info-krizek');
+  } else {
+    event.target.classList.add('info-kolecko');
+  }
+
+  player = !player;
+};
+const infoElm = document.querySelector('.info').addEventListener('click', Info);
