98082126c778ce4707251327ca7394b0d9949143
Magdalena H. Selingerová <selingerova.magdalena@gmail.com>
2020-10-23 15:32:09 +0200

    final version

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-console.log('funguju');
-
-//přidat na kliknutí do pole kolečko
-
-/*const circle = event.target.classList.add('board__field--circle');*/
-
-/* const cross = document
-  .querySelector('.info')
-  .removeClass('info-kolecko')
-  .addClass('info-krizek'); */
-
 let player = false;
+
+const currentPlayer = document.querySelector('.info');
+
 const Play = (event) => {
   if (player) {
     event.target.classList.add('board__field--cross');
     event.target.setAttribute('disabled', true);
+    currentPlayer.classList.remove('info-kolecko');
+    currentPlayer.classList.toggle('info-krizek');
   } else {
     event.target.classList.add('board__field--circle');
     event.target.setAttribute('disabled', true);
+    currentPlayer.classList.remove('info-krizek');
+    currentPlayer.classList.toggle('info-kolecko');
   }
   player = !player;
 };
@@ -25,13 +21,3 @@ const btnElm = document.querySelectorAll('#btn_play');
 for (let i = 0; i < btnElm.length; i += 1) {
   btnElm[i].addEventListener('click', Play);
 }
-const Info = (event) => {
-  if (player) {
-    event.target.classList.add('info-krizek');
-  } else {
-    event.target.classList.add('info-kolecko');
-  }
-
-  player = !player;
-};
-const infoElm = document.querySelector('.info').addEventListener('click', Info);
